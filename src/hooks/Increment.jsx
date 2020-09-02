77f8a5ed9f6daa0842df884cf729186848033a4c
suhailphp <suhailphp@gmail.com>
2020-09-02 10:39:49 +0400

    Name input added in functional component

--- src/hooks/Increment.jsx
+++ src/hooks/Increment.jsx
@@ -2,9 +2,19 @@ import React, { Fragment, useState } from "react";
 
 const Increment = () => {
   const [value, setValue] = useState(0);
+  const [name, setName] = useState("");
   return (
     <Fragment>
-      <div>Increment Value is {value}</div>
+      <input
+        type="text"
+        name="name"
+        placeholder="Your Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <div>
+        Hello {name} ,Increment Value is {value}
+      </div>
       <button onClick={() => setValue(value + 1)}>Increment</button>
     </Fragment>
   );
