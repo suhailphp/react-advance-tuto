413778d2e5c0e87ad4930dcc106c5b8c49e231d1
suhailphp <suhailphp@gmail.com>
2020-09-02 17:41:52 +0400

    Using Context inside component did mount function

--- src/context/MovieList.jsx
+++ src/context/MovieList.jsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import UserContext from "./userContext";
 
 class MovieList extends Component {
+  static contextType = UserContext;
+  componentDidMount() {
+    console.log("context", this.context);
+  }
   render() {
     return (
       <UserContext.Consumer>
